f3b0b6c7a63431521b3ecdf8c0a82b3f4e53358b
Oleg Tarusov <ot88899888@gmail.com>
2019-06-08 12:36:58 +0300

    tables Univercity, Specialization, UnivercitySpecialization

--- Backend/Application/Models/Specialization.cs
+++ Backend/Application/Models/Specialization.cs
@@ -5,13 +5,10 @@ using System.Threading.Tasks;
 
 namespace Application.Models
 {
-    public class Person
+    public class Specialization
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Surname { get; set; }
-        public string PatronymicName { get; set; }
-        public DateTime BirthDate { get; set; }
-        public Univercity Univercity { get; set; }
+        public string Code { get; set; }
     }
 }
