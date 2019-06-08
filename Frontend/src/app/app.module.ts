59f3b219f20d944874423cb3a6af13b6d7111258
Andrei Fedotov <andrei_fedotov@tutamail.com>
2019-06-08 21:07:06 +0300

    add front stuff

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,24 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatButtonModule, MatCheckboxModule, MatInputModule, MatToolbarModule } from '@angular/material';
+import { AppComponent } from './app.component';
 import { DocumentsComponent } from './documents/documents.component';
+import { NavComponent } from './common/nav/nav.component';
+import { UniversityComponent } from './university/university.component';
+
+import { MaterialModule } from './material/material.module';
+import { UniversityListComponent } from './university/university-list/university-list.component';
+import { UniversityFilterComponent } from './university/university-filter/university-filter.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { LinksComponent } from './common/links/links.component';
+import { RatingComponent } from './rating/rating.component';
+import { GeneralComponent } from './general/general.component';
+
+const appRoutes: Routes = [
+  { path: 'general', component: GeneralComponent },
+  { path: 'university', component: UniversityComponent },
+  { path: 'mydocs', component: DocumentsComponent },
+  { path: 'rating', component: RatingComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    DocumentsComponent
+    DocumentsComponent,
+    NavComponent,
+    UniversityComponent,
+    UniversityListComponent,
+    UniversityFilterComponent,
+    HeaderComponent,
+    FooterComponent,
+    LinksComponent,
+    RatingComponent,
+    GeneralComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatInputModule,
-    MatToolbarModule,
+    MaterialModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
